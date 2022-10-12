bd562dcb2c5ecb07e6f884b0cec4589a8e0e983d
Alexandresfi <alexandre.sfi@hotmail.com>
2022-10-11 23:25:34 -0300

    create footer

---
+++ src/Containers/Footer/index.tsx
@@ -0,0 +1,9 @@
+import { ContainerFooter } from "./styles";
+
+export function Footer() {
+  return (
+    <ContainerFooter>
+      <h1>Footer</h1>
+    </ContainerFooter>
+  );
+}
---
+++ src/Containers/Footer/styles.ts
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const ContainerFooter = styled.footer`
+    text-align: center;
+`
